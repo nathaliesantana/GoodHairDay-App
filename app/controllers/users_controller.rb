ef7a425840ea482c7286ff1481cca8f814b98910
github username <github email address>
2020-03-07 12:05:23 -0500

    Defined User params under private method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
 
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        
-    end
+  def create
 
-    def show
-        
-    end
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :hair_texture, :hair_structure, :scalp_moist)
+  end
+
 end
