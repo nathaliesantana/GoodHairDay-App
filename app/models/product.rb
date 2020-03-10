71d57dff8ed4653bbcf40976cba6938c34edad3f
github username <github email address>
2020-03-10 07:14:17 -0400

    Updated associations for a succesfully many to many relationship.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :reviews
-  has_many :users
+  has_many :users, :through => :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :products
     has_many :reviews
-    has_many :categories, through: :products
+    has_many :products, :through => :reviews
+    #has_many :categories, :through => :products
     has_secure_password
+
 end
