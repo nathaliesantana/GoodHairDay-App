f6dd78378423ab496cd5376d44c74c06a0b3bc18
github username <github email address>
2020-03-17 22:48:36 -0400

    Created LogOut Method.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,5 +7,9 @@ module SessionsHelper
     @current_user = User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
 end
