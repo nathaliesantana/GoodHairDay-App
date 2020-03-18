01fd1aeb7625580728f9e10ca787eb324f231dcd
github username <github email address>
2020-03-17 22:50:34 -0400

    Home page renders correctly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'sessions#home'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
-  get '/newproduct' => 'products#new'
-  post '/product' => 'products#create'
-
-
+  delete '/logout' => 'sessions#destroy'
 
   resources :sessions
   resources :categories
