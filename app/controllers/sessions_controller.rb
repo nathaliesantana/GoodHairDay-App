13406d0fda31580476ff2fff3484df783f781eaa
github username <github email address>
2020-03-19 06:40:16 -0400

    User needs to enter a valid password to Log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,13 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = @user.try(:authenticate, params[:user][:password])
+    if @user
       log_in @user
       redirect_to @user
     else
       flash[:danger] = 'Invalid name/password combination'
-      redirect_to @user
+      redirect_to '/'
     end
   end
 
