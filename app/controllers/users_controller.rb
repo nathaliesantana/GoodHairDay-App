9ee27f9b3798372d0f9b25a9c733cd51879d6384
github username <github email address>
2020-04-07 01:12:50 -0400

    Users are able to edit their information.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+
+    @user = User.find_by_id(params[:id])
+
+    @user.update(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
 
   private
   def user_params
