bf1fa90910da0a8538f1fc8fdb1ed6dfebe48a3b
github username <github email address>
2020-04-16 21:17:02 -0400

    User now needs to login with email instead of username.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
+    @user = User.find_by(email: params[:user][:email])
     @user = @user.try(:authenticate, params[:user][:password])
     if @user
       log_in @user
