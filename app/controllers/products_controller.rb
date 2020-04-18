00b64484ed1d8bec6a0bc9f5ffea9f4dcc92e9d3
github username <github email address>
2020-04-18 15:29:25 -0400

    Eliminate ability to create a product by itself, without a review.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,18 +14,4 @@ class ProductsController < ApplicationController
     render :new if !@product
   end
 
-  def create
-    @product = Product.find_or_create_by(product_params)
-    if @product
-      redirect_to @product
-    else
-      flash[:error]
-      render :new
-    end
-  end
-
-  private
-  def product_params
-    params.require(:product).permit(:name, :brand, :category_id)
-  end
 end
