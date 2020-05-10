d6b85841ee5b0db40f77b9b43f56dd2df3e87041
NathalieSantana <nathalie.scruz@gmail.com>
2020-05-10 00:53:52 -0400

    Added Categories and Admin.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#user
+admin = User.create(email: "admin@goodhairday.com", username: "admin",  password: "adminpassword")
+
+#category
+shampoo = Category.create(name: "Shampoo")
+conditioner = Category.create(name: "Conditioner")
+deep_conditioner = Category.create(name: "Deep Conditioner")
+oil = Category.create(name: "Oil")
+hair_mousse = Category.create(name: "Hair Mousse")
+heat_protectant = Category.create(name: "Heat Protectant")
